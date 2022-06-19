71191cdaa8e92abaec116fa023aa647a10e32c2f
Rohit_shinde <rohit5521369@gmail.com>
2022-06-19 19:49:17 +0530

    first commit

---
+++ project-jenkins-pipeline/project-jenkins-pipeline/src/main/java/com/example/projectjenkinspipeline/ProjectJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJenkinsPipelineApplication.class, args);
+	}
+
+}
---
+++ project-jenkins-pipeline/project-jenkins-pipeline/src/test/java/com/example/projectjenkinspipeline/ProjectJenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectjenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
